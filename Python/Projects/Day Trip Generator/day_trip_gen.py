123a04f336c2360ed1cd23a455636254864b1089
Mario Lugo <mlugo23699@gmail.com>
2023-11-08 14:57:30 -0800

    consolidated all the random functions into one, defined a day day trip generator function that calls the eandom element function for each list and prints the randomized trip, working on creating a while loop with conditionals in it to verify if the user is happy with their trip

--- Python/Projects/Day Trip Generator/day_trip_gen.py
+++ Python/Projects/Day Trip Generator/day_trip_gen.py
@@ -6,20 +6,27 @@ transportation_list = ['taxi', 'metro', 'bicycle rental', 'on foot', 'Skytrain',
 entertainment_list=['Mamut cerveceria', 'Lacro Museum', 'Canal Cruise', 'Cairo Citadel', 'Siam Park', 'Flemington Racecourse']
 
 
-print("\nWelcome!\n\nLooking for a completely random travel experience?\nYou've come to the right place!\n\nNow, lets get to mixin'!")
+print("\nWelcome!\n\nLooking for a completely random travel experience?\nYou've come to the right place!\nNow, lets get to mixing!\n")
 
-def rand_location(location_list):
-    location = random.choice(location_list)
-    return location
+def rand_element(e):
+    element = random.choice(e)
+    return element
 
-def rand_restaurant(grub_list):
-    grub = random.choice(grub_list)
-    return grub
+def trip_generator():
+    location = rand_element(city_list)
+    grub = rand_element(eats_list)
+    vehicle = rand_element(transportation_list)
+    entertainment = rand_element(entertainment_list)
+    return f"Destination: {location}\nFood: {grub}\nMode of Transportation: {vehicle}\nForm of Entertainment: {entertainment}\n"
 
-def rand_transportation(vehicle_list):
-    vehicle = random.choice(vehicle_list)
-    return vehicle
+user_input = input(f"\nHere's your completely randomized trip itinerary.\n\n{trip_generator()}\n\nAre you satisfied with your current trip?\nType yes or no\n\n")
 
-def rand_entertainment(entertain_list):
-    entertainment = random.choice(entertain_list)
-    return entertainment
+user_satisfied = False
+while user_satisfied != True:
+    if user_input == "yes":
+        print("\nGreat, hope you have tons of fun!")
+        user_satisfied = True
+    elif user_input != "yes":
+        user_change = input("\nSorry to hear that, what part would you like to change?\nLocation? Food? Vehicle? Entertainment? ")
+        if user_change == "Location":
+            
